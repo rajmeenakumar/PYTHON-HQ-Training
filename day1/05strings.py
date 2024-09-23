a1446a071c1ac8f5ca5c406f52e3aa0cde08648b
Puneet Vashisht <puneetvashisht@gmail.com>
2024-09-23 10:54:54 +0530

    test

---
+++ day1/05strings.py
@@ -0,0 +1,20 @@
+name = "John Doe"
+age = 30
+city = "New York"
+
+print(" Name is " + name)
+print(f"My name is {name}, I am {age} years old, and I live in {city}.")    
+
+multi_line_string = """
+    This is a multi-line string.
+    It can span multiple lines.
+    Each line is indented by four spaces.
+"""
+
+str = "Hello, World! \nThis is a new line. \tThis is a tab."
+
+str.upper()
+str.replace("Hello", "Goodbye")
+str
+
+print(multi_line_string)
