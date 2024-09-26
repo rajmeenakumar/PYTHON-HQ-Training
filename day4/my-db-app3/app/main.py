72167ce5e742142c99c0f41cc3f7f91b7e18b730
Puneet Vashisht <puneetvashisht@gmail.com>
2024-09-26 13:06:17 +0530

    authentication with jwt

--- day4/my-db-app3/app/main.py
+++ day4/my-db-app3/app/main.py
@@ -1,9 +1,10 @@
-from fastapi import FastAPI, HTTPException, Depends
+from fastapi import FastAPI, HTTPException, Depends, Request
 from app.routers import destinations, category, auth
 from . import models
 from . import database
 from app.middlewares.logging_middleware import logging_middleware
 from app.middlewares.jwt_middleware import jwt_middleware
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 
@@ -18,3 +19,15 @@ app.include_router(destinations.router, prefix="/api/v1/destinations")
 app.include_router(category.router, prefix="/api/v1/categories")
 app.include_router(auth.router, prefix="/api/v1/auth")
 
+@app.exception_handler(Exception)
+async def custom_exception_handler(request: Request, exc: Exception):
+    # print(exc.status_code)
+    if exc.status_code:
+        return JSONResponse(
+            status_code=exc.status_code,
+            content={"message": str(exc)},
+        )
+    return JSONResponse(
+        status_code= 500,
+        content={"message": "An unexpected error occurred", "details": str(exc)},
+    )
--- day4/my-db-app3/app/middlewares/jwt_middleware.py
+++ day4/my-db-app3/app/middlewares/jwt_middleware.py
@@ -6,20 +6,30 @@ from app.utils.jwt_handler import verify_token
 # logging.basicConfig(level=logging.INFO , filename='app.log', filemode='a', format='%(asctime)s - %(levelname)s - %(message)s')
 
 async def jwt_middleware(request: Request, call_next):
-    print("JWt middleware called.....")
-    token = request.headers.get('Authorization')
+
+    public_paths = ["/api/v1/auth/login", "/api/v1/auth/register", "/api/v1/categories/", "/docs","/openapi.json", "/api/v1/categories" ]
+
+    if request.url.path in public_paths:
+        return await call_next(request)
+
+    token = request.headers.get("Authorization")
+
+    # if not token:
+    #     raise HTTPException(status_code=401, detail="Authorization token required")
+
+    # print(token)
     
-    if token and token.startswith('Bearer '):
-        print(token)  # Remove 'Bearer ' from the token string to get the actual token)
-        token = token.replace('Bearer ', '')
+    if token and token.startswith("Bearer "):
+        token = token.split(" ")[1]
         payload = verify_token(token)
         if payload:
             request.state.user = payload
+            
         else:
-            raise HTTPException(status_code=401, detail="Invalid token")
+            raise HTTPException(status_code=401, detail="Invalid or expired token")
     else:
-        print("Missing or invalid token")
-        raise HTTPException(status_code=401, detail="Missing or invalid token")
-    
+        request.state.user = None
+        raise HTTPException(status_code=401, detail="Invalid or expired token")
+
     response = await call_next(request)
     return response
